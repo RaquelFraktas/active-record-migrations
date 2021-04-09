abfcc985816dd67c1cdbd8e48204e0e183b992fe
github username <github email address>
2021-04-09 11:37:30 -0400

    commit tree 6449178ebe70cd4627936d809cc81d513fac8317

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-    def up
-    end
-  
-    def down
-    end
+    # def up
+    # end
+  #####the issue may have been having both an up and a change method in the migration. We want to either use up/down, or change. But you can pretty much always use change
+    # def down
+    # end
 
     def change
       create_table :artists do |t|
